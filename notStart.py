d3f20a27782d4bfb4d31c76e9f964838ac8e2369
Nick <thisisnickteoh@g.ucla.edu>
2020-04-09 21:32:49 -0700

    added merge

--- notStart.py
+++ notStart.py
@@ -3,8 +3,8 @@ from pprint import pprint as pprint
 
 a = [1, 7, 15, 4, 5, 12, 0, -2]
 b = []
-c = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-d = [7, 4, 200, 120]
+d = [1,2,3,4,5]
+c = [8,9]
 e = [-1, 4, 120000000, 23, -988888888]
 f = [100, -4]
 test_array = [a, b, c, d, e, f]
@@ -31,10 +31,48 @@ def bubblesort(a_list): # BAD BECAUSE OF N^2 TIME COMPLEXITY
 
 
 
-for i in range(len(test_array)):
-	print(i)
-	bubblesort(test_array[i])
+# for i in range(len(test_array)):
+# 	print(i)
+# 	bubblesort(test_array[i])
 	# assert test_array[i] == sorted(test_array[i]), "{} is not sorted".format(test_array[i])
 	# assert False
 
 # print(test_array)
+
+def merge(list1, list2):
+	i1 = 0
+	i2 = 0
+	res = []
+
+	for _ in range(0,len(list1)+len(list2)):
+		# compare element in list 1 to element in list 2
+		# if list 2 is over, append list 1
+		# if list 1 is over, append list 2
+		# if list 1 element is lower or equals, put in list 1 element, increment list 1
+		# if list 2 element is lower or equals, put in list 2 element, increment list 2
+		
+
+		if i1 >= len(list1) or i2 < len(list2) and list2[i2] <= list1[i1]: # short circuiting won't check second condition in "and" "or" if the first one passes/fails
+			print("just list2 append")
+			print("i2", i2)
+			res.append(list2[i2])
+			i2 += 1
+		else: # i2 >= len(list2) or list1[i1] <= list2[i2]:
+			print("just list1 append")
+			print("i1", i1)
+			res.append(list1[i1])
+			i1 += 1
+		# elif list1[i1] <= list2[i2]:
+		# 	res.append(list1[i1])
+		# 	print("i1", i1)
+		# 	i1 += 1
+		# elif list2[i2] <= list1[i1]:
+		# 	res.append(list2[i2])
+		# 	print("i2", i2)
+		# 	i2 += 1
+		
+		print(res)
+
+	return res
+
+pprint(merge(c, d))
